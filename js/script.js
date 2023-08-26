cf0faf839ffa8602f031d7855f7132e9fea470fd
Ignacio P <51886210+Pinto2291@users.noreply.github.com>
2023-08-25 21:07:33 -0400

    completed proyect

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let playerOne = true;
+let celda = document.getElementsByClassName('celda');
+
+for(let i = 0; i < celda.length; i++){
+    celda[i].addEventListener('click', gameStart);
+}
+
+function gameStart(evento) {
+    let valorCelda = evento.target.innerHTML;
+
+    if(!valorCelda.length){
+        evento.target.innerHTML = playerOne ? 'X' : 'O';
+        playerOne = !playerOne;
+
+        // Validacion horizontal
+        checkLine(0, 1, 2);
+        checkLine(3, 4, 5);
+        checkLine(6, 7, 8);
+
+        // Validacion Vertical
+        checkLine(0, 3, 6);
+        checkLine(1, 4, 7);
+        checkLine(2, 5, 8);
+
+        //Validacion Diagonal
+        checkLine(0, 4, 8);
+        checkLine(6, 4, 2);
+    }
+}
+
+function checkLine(celda1, celda2, celda3){
+    if(
+        celda[celda1].innerHTML.length && 
+        celda[celda1].innerHTML == celda[celda2].innerHTML && 
+        celda[celda2].innerHTML == celda[celda3].innerHTML) {
+            showWinner(celda[celda1].innerHTML);
+        }
+}
+
+function showWinner(player){
+    document.querySelector('#resultado').innerHTML = player + ' Ganador!'
+}
